869a76a289b7a3f540364a88b5b6dfb56e004753
Vince <vincentzhang1996@gmail.com>
2014-02-21 19:15:02 -0500

    Changed DataCorruptedException to extend RuntimeException instead of IOException.

--- src/org/phoenix/assetdatabase/DataCorruptedException.java
+++ src/org/phoenix/assetdatabase/DataCorruptedException.java
@@ -1,12 +1,10 @@
 package org.phoenix.assetdatabase;
 
-import java.io.IOException;
-
 /**
  *
  * @author Vince
  */
-public class DataCorruptedException extends IOException {
+public class DataCorruptedException extends RuntimeException {
 
     public DataCorruptedException(String message) {
         super(message);
